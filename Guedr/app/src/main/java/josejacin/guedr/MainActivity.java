a2e6b0dd0127dc8ad380b9af24933e3ad5713c27
José Sánchez <joseynene@hotmail.com>
2017-05-17 22:31:23 +0200

    Example of Log and saveInstanceState

--- Guedr/app/src/main/java/josejacin/guedr/MainActivity.java
+++ Guedr/app/src/main/java/josejacin/guedr/MainActivity.java
@@ -2,12 +2,26 @@ package josejacin.guedr;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+    // Para coger el nombre de la clase
+    protected static String TAG = MainActivity.class.getCanonicalName();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.v(TAG, "Hola Amundio, he pasado por onCreate");
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        Log.v(TAG, "Nos han llamado a onSaveInstanceState");
+        outState.putString("clave", "valor");
     }
 }
